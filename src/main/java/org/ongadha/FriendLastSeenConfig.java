42349c36cde8be78afb008c042073d49deeb04ad
on-gadha <samuel.osterberg@hotmail.com>
2025-10-02 13:28:28 +0200

    fixed normal setup

--- src/main/java/org/ongadha/FriendLastSeenConfig.java
+++ src/main/java/org/ongadha/FriendLastSeenConfig.java
@@ -1,11 +1,11 @@
-package com.example;
+package org.ongadha;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
-@ConfigGroup("example")
-public interface ExampleConfig extends Config
+@ConfigGroup("FriendLastSeen")
+public interface FriendLastSeenConfig extends Config
 {
 	@ConfigItem(
 		keyName = "greeting",
@@ -14,6 +14,6 @@ public interface ExampleConfig extends Config
 	)
 	default String greeting()
 	{
-		return "Hello";
+		return "abow";
 	}
 }
--- src/main/java/org/ongadha/FriendLastSeenPlugin.java
+++ src/main/java/org/ongadha/FriendLastSeenPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package org.ongadha;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "FriendLastSeen"
 )
-public class ExamplePlugin extends Plugin
+public class FriendLastSeenPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private FriendLastSeenConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	FriendLastSeenConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(FriendLastSeenConfig.class);
 	}
 }
